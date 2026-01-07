c22d691f86ac1f03ecda57185bc38bdd9aeddde2
vedika82 <anandvedika420@gmail.com>
2026-01-07 23:12:45 +0530

    added buttons

--- src/Components/ContentRating.jsx
+++ src/Components/ContentRating.jsx
@@ -5,11 +5,20 @@ import './ContentRating.css';
 class ContentRating extends Component {
   constructor() {
     super();
+     this.state = {
+      likes: 0,
+      dislikes: 0
+    };
   }
   render() {
     return (
      <>
-     <h1>Text Content Rating</h1>
+    <div className='content-rating'>
+    <p>
+    i am happy because i am sad
+    </p>
+     <div className='rating-buttons'></div>
+    </div>
      </>
     );
   }
