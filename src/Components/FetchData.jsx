abf92bdd0c8a4ffc840bd0785c4e7f48dad34e2d
vedika82 <anandvedika420@gmail.com>
2026-01-11 22:44:51 +0530

    fetching data from an api and showing selective data

--- src/Components/FetchData.jsx
+++ src/Components/FetchData.jsx
@@ -1,10 +1,33 @@
 import React from 'react'
+import useFetch from './UseFetch'; //imported useFetch custom hook
+import './FetchData.css'
 
 const FetchData = () => {
+  // Implement the custom hook by passing the specific url from which you want to fetch data inside the FetchData arrow function. Also include console.log to check whether data has been retrieved in the variable data
+  const [data]=useFetch('https://api.npoint.io/9045c260b1565daa9e15');
+  console.log(data);
   return (
     <>
+{/* Now you need to fetch data from API Data Link. There are two approaches to fetch the data: */}
+{/* First, implement the logic using the fetch API method within each component that needs to fetch data. Suppose you have 5 components in which you need to load the data from external source. In this case, for each component you need to write the similar logic to fetch the details. */}
+{/* Second, create a reusable custom hook. This custom hook encapsulates the data fetching logic, allowing it to be reused across multiple components without the need to rewrite the entire code to fetch details in each component. */}
+      <h1 className='usefetch_heading'>Use Fetch Custom Hook</h1>
      <ul className='list_data_main'>
-        <h1 className='usefetch_heading'>Use Fetch Custom Hook</h1>
+        {/* To retrieve the data, you need to perform an iteration of the data array to fetch the entire data. You need to apply the map array method  */}
+         {data && data.map((e, index)=>(
+      <>
+      {/* here we have multiple data in the console thata needs to be fetched  */}
+      {/* selective data fetched */}
+      {/* These distinct types, then can fetch data using "e" variable that you have passed as parameter in the map method. */}
+      {/* Note: The e parameter contains the value of each array index for every iteration. */}
+      <li key={index} className='lsit_data'>
+        <h3>{e.name}</h3>
+        <p><strong>Importance: </strong>{e.importance}</p>
+        <p><strong>Benefits: </strong>{e.benefits}</p>
+        <p><strong>Time to eat: </strong>{e.best_time_to_intake}</p>
+      </li>
+      </>
+    ))}
      </ul>
     </>
   )
--- src/Components/UseFetch.jsx
+++ src/Components/UseFetch.jsx
@@ -1,5 +1,18 @@
-const UseFetch = (url) => {
-    
-}
 
+// In the above example, an arrow function is declared that takes a single parameter named url.
+// This method has been export as default.
+import {useState ,useEffect } from 'react'
+// The url variable is passed as a parameter in the useFetch arrow function to specify the exact URL of a specific website from which any component can easily fetch data
+const UseFetch = (url) => {
+    const [data, setData]= useState();
+     useEffect(()=>{
+        fetch(url).then((res)=>res.json())
+        .then((data)=>setData(data))
+    },[])
+return [data]
+}
+// useEffect: This is a React Hook used for performing side effects in functional components. It’s typically used for fetching data, subscribing to events, or any other side effects that don’t involve rendering.
+// fetch(url): This initiates an HTTP request to the specified url.
+// .then((res) => res.json()): This converts the response from the server to JSON format.
+// .then((data) => setData(data)): This sets the retrieved data to the state variable data. setData is a function that updates the state in React functional components.
 export default UseFetch
